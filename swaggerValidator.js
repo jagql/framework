7afa776cc53ded89f193ff986053e2b580bec19f
Arnav Gupta <arnav@codingblocks.com>
2018-01-28 10:34:29 +0530

    fix lint
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- swaggerValidator.js
+++ swaggerValidator.js
@@ -1,47 +1,47 @@
-const jsonApiTestServer = require("./example/server.js");
-const request = require("request");
-const assert = require("assert");
+const jsonApiTestServer = require('./example/server.js')
+const request = require('request')
+const assert = require('assert')
 
-describe("Use a tool to validate the generated swagger document", () => {
-  it("should not contain any errors", done => {
-    const validator = require("swagger-tools").specs.v2;
+describe('Use a tool to validate the generated swagger document', () => {
+  it('should not contain any errors', done => {
+    const validator = require('swagger-tools').specs.v2
 
-    const uri = "http://localhost:16006/rest/swagger.json";
+    const uri = 'http://localhost:16006/rest/swagger.json'
     request(uri, (meh, res, swaggerObject) => {
-      swaggerObject = JSON.parse(swaggerObject);
+      swaggerObject = JSON.parse(swaggerObject)
 
       validator.validate(swaggerObject, (err, result) => {
-        assert.ifError(err);
+        assert.ifError(err)
 
         if (!result) {
-          console.log("Swagger document is valid");
-          return done();
+          console.log('Swagger document is valid')
+          return done()
         }
 
         if (result.errors.length > 0) {
-          console.log("The Swagger document is invalid...");
-          console.log("");
-          console.log("Errors");
-          console.log("------");
-          console.log(result.errors);
-          console.log("");
+          console.log('The Swagger document is invalid...')
+          console.log('')
+          console.log('Errors')
+          console.log('------')
+          console.log(result.errors)
+          console.log('')
         }
 
         if (result.warnings.length > 0) {
-          console.log("Warnings");
-          console.log("--------");
-          console.log(result.warnings);
+          console.log('Warnings')
+          console.log('--------')
+          console.log(result.warnings)
         }
 
-        done(new Error("Invalid swagger.json!"));
-      });
-    });
-  });
+        done(new Error('Invalid swagger.json!'))
+      })
+    })
+  })
 
   before(() => {
-    jsonApiTestServer.start();
-  });
+    jsonApiTestServer.start()
+  })
   after(() => {
-    jsonApiTestServer.close();
-  });
-});
+    jsonApiTestServer.close()
+  })
+})
