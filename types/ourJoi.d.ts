a39be5665500292860ee2c6419bd3731dc2c363b
Arnav Gupta <arnav@codingblocks.com>
2018-02-06 18:04:43 +0530

    fix our joi schema
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- types/ourJoi.d.ts
+++ types/ourJoi.d.ts
@@ -1,9 +1,15 @@
-import {Schema} from 'joi'
+import {AnySchema, JoiObject, Schema} from 'joi'
 import Joi = require('joi')
 
+type UidType = 'uuid' | 'autoincrement'
+
+interface OurJoiSchema extends AnySchema {
+  uidType: (type: UidType) => this
+}
+
 interface OurJoi extends Joi.Root {
-  one: (...model: string[]) => Schema
-  many: (...model: string[]) => Schema
+  one: (...model: string[]) => OurJoiSchema
+  many: (...model: string[]) => OurJoiSchema
 }
 
 export = OurJoi
